542aaed5e2f76296f1e771b9faf14818b354489a
Ammarlio <ammarhalbouni@Gamil.com>
2018-08-15 16:46:20 +0300

    Post && Get Methods for adding and rendering the cats

--- angular-client/components/catsList.js
+++ angular-client/components/catsList.js
@@ -4,15 +4,43 @@ app.component('cats', {
 	templateUrl: '/templates/adoptCat.html'
 });
 app.controller('catsCtrl', function ($scope, $http,$window){
+ 
 var getCats = function() {
-	// TODO: Your code here
+	//get the cats from server side
+	$http.get('/cats')
+  .then(function (response) {
+    // handle success
+    console.log(response)
+    $scope.cats = response.data
+    console.log('bsbs',$scope.cats)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
 
 };
-getCats();
+getCats();//fetch cats on call
 
 $scope.addCats=function() {
-// TODO: Your code here
-
+  //add the cats from client side and send them to server side to be saved inside the database
+$http.post('/cats', {
+    catName: $scope.catName,
+    ownerEmail: $scope.ownerEmail,
+    imageUrl : $scope.imageUrl,
+    adoptionMessage : $scope.adoptionMessage
+  })
+  .then(function (response) {
+    console.log(response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+	
 
 }
 
