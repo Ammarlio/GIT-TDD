4b7dd542862b4d80b33832dd6864bee49dec7e06
Ammarlio <ammarhalbouni@Gamil.com>
2018-08-15 16:45:26 +0300

    catlist testing category

--- test/components/catsListSpec.js
+++ test/components/catsListSpec.js
@@ -1,1 +1,10 @@
 // TODO: Your code here
+describe('ValueService', () => {
+  let service: ValueService;
+  beforeEach(() => { service = new ValueService(); });
+ 
+  it('#getValue should return real value', () => {
+    expect(service.getValue()).toBe('real value');
+  });
+
+)}
